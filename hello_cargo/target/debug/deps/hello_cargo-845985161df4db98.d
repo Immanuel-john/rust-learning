6a9e578217473a5e7b7d51183f6c5ff76eed9820
Immanuel John <imjjohn18@gmail.com>
2021-08-09 11:02:22 +0530

    cargo hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo-845985161df4db98.d
@@ -0,0 +1,5 @@
+/home/immanuel/works/rust-learning/hello_cargo/target/debug/deps/hello_cargo-845985161df4db98: src/main.rs
+
+/home/immanuel/works/rust-learning/hello_cargo/target/debug/deps/hello_cargo-845985161df4db98.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/immanuel/works/rust-learning/hello_cargo/target/debug/hello_cargo: /home/immanuel/works/rust-learning/hello_cargo/src/main.rs
