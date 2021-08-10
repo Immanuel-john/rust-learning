5190f5a72c34429e97e7b024b057993213bc0d8a
Immanuel John <imjjohn18@gmail.com>
2021-08-10 08:58:23 +0530

    complete guessing game with external dependency rand

--- guessing_game/target/debug/deps/guessing_game-6b70b6e934b8efd0.d
+++
@@ -1,5 +0,0 @@
-/home/immanuel/works/rust-learning/guessing_game/target/debug/deps/guessing_game-6b70b6e934b8efd0: src/main.rs
-
-/home/immanuel/works/rust-learning/guessing_game/target/debug/deps/guessing_game-6b70b6e934b8efd0.d: src/main.rs
-
-src/main.rs:
