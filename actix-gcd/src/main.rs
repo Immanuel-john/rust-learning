f074e0ed373405ca60c3473d49d95b2097569710
Immanuel-john <imjjohn18@gmail.com>
2022-07-12 19:52:14 +0530

    median

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -17,6 +17,8 @@ struct GcdParameters {
     m: u64,
 }
 
+
+
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
---
+++ median/src/main.rs
@@ -0,0 +1,56 @@
+fn median(mut a: Vec<f32>) -> Option<f32> {
+    a.sort_by(|x, y| x.partial_cmp(y).unwrap());
+    if a.is_empty() {
+        None
+    }
+    else {
+        let length = a.len();
+        let mid = a.len() / 2;
+        
+        if length % 2 == 0 {
+            Some((a[mid - 1] + a[mid])/2.0)
+        }
+        else {
+            Some(a[mid])
+        }
+
+    }
+}
+
+fn main() {
+    let answer = median(vec![1.0, 2.0, 5.0]);
+
+    println!("median([1,2,5]) = {:?}", answer);
+}
+
+#[test]
+fn empty_list() {
+    let input = vec![];
+    let expected_output = None;
+    let actual_output = median(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn sorted_list() {
+    let input = vec![1.0, 4.0, 5.0];
+    let expected_output = Some(4.0);
+    let actual_output = median(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn even_length() {
+    let input = vec![1.0, 3.0, 5.0, 6.0];
+    let expected_output = Some(4.0);
+    let actual_output = median(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn unsorted_list() {
+    let input = vec![1.0, 5.0, 2.0];
+    let expected_output = Some(2.0);
+    let actual_output = median(input);
+    assert_eq!(actual_output, expected_output);
+}
